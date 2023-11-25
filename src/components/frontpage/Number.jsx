3e00eb722dcc6d1cf54a551948398d624b58d7f1
slavenSusic <118363308+slavenSusic@users.noreply.github.com>
2023-11-25 11:42:45 +0100

    aaa

--- src/components/frontpage/Number.jsx
+++ src/components/frontpage/Number.jsx
@@ -14,7 +14,7 @@ function Number({ n, title, checkmark }) {
      <div class="flex justify-center py-0 z-20">
        <div class="flex flex-col items-center justify-center ml-4">
       
-        <div class="flex text-base md:text-3xl items-center space-x-1 justify-center dark:text-gray-100 z-10">
+        <div class="flex text-base md:text-3xl items-center space-x-1 justify-center text-gray-100 z-10">
           <animated.div class="text-5xl md:text-5xl font-bold">{number.to((n) => n.toFixed(0))}</animated.div>
           <p class="text-lg">+</p>
         </div>
