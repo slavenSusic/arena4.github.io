99692c99f62c6ea1e92ea53e79758d78cd39c340
slavenSusic <118363308+slavenSusic@users.noreply.github.com>
2023-11-27 14:32:59 +0100

    pages3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercelServerless from '@astrojs/vercel/serverless';
+// import vercelServerless from '@astrojs/vercel/serverless';
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
