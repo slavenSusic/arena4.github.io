a2507799ab38ef414123b22552ad94bf8ddeea1c
slavenSusic <118363308+slavenSusic@users.noreply.github.com>
2023-11-27 14:30:57 +0100

    pages2

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-   output: "server",
+  //  output: "server",
   // adapter: vercelServerless(),
   site: 'https://slavenSusic.github.io',
  
